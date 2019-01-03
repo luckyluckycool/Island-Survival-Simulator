43484f4310774b91b6be19e41262d35bc465000e
luckyluckycool <gonsor.y2@gmail.com>
2019-01-03 16:25:05 +0200

    1

--- myGame/app/src/main/java/luckycoolgames/mygame/MainActivity.java
+++ myGame/app/src/main/java/luckycoolgames/mygame/MainActivity.java
@@ -2,12 +2,15 @@ package luckycoolgames.mygame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
     }
 }

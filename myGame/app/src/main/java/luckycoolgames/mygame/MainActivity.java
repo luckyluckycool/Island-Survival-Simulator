c33d6605cf1db23f6d3989aaba9b7dadebce69ee
luckyluckycool <gonsor.y2@gmail.com>
2019-01-04 16:00:03 +0200

    new_game_button added

--- myGame/app/src/main/java/luckycoolgames/mygame/MainActivity.java
+++ myGame/app/src/main/java/luckycoolgames/mygame/MainActivity.java
@@ -1,16 +1,27 @@
 package luckycoolgames.mygame;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
+textView = findViewById(R.id.new_game_button);
+textView.setOnTouchListener(new View.OnTouchListener() {
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        textView.animate().scaleX((float)0).scaleY((float)0).setDuration(1700).start();
+        return false;
+    }
+});
     }
 }
